c2607dce357088931205406b935be6c9081841b8
Zihao Ye <49857367+Eyizoha@users.noreply.github.com>
2024-03-01 11:19:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from tchisla import TchislaSolver
 from math import factorial
 
-target = 1884
+target = 2016
 optimal_solutions = [None, 9, 6, 4, 4, 6, 4, 6, 5, 4]
 
 for i in range(1, 10):
   ts = TchislaSolver(target, i)
   result = ts.solve(trace=True)
-  # assert str(result).count(str(i)) == optimal_solutions[i]
+  assert str(result).count(str(i)) == optimal_solutions[i]
   assert target == round(eval(result.evaluable()))
   print('{} = {}'.format(target, result))
   print()
